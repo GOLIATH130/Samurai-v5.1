450d08b75e1dc78599df7c7e79fee3fee8fa1793
GOLIATH130 <70407233+GOLIATH130@users.noreply.github.com>
2021-05-19 17:06:40 -0500

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,71 @@
+const help = (prefix) => {
+	return `
+「 *𝕤𝕒𝕞𝕦𝕣𝕒𝕚 𝕓𝕠𝕥* 」
+
+◪ *INFORMACION*
+  ❏ Comando: 「  ${prefix}  」
+  ❏ Creador: Samurai
+
+◪ *PARA USAR EL BOT*
+  ❏ Registrate  ${prefix}daftar y tu nombre
+
+◪ *CREAR STICKERS*
+  │
+  ├─ ❏ ${prefix}sticker o s
+  ├─ ❏ ${prefix}stickergif
+  ├─ 6 segundos de video
+  ├─ ❏ ${prefix}toimg
+  ├─ De sticker a JPG
+  ├─ ❏ ${prefix}tomp3
+  └─ De video a MP3
+
+◪ *DESCARGAS*
+  │
+  ├─ ❏ ${prefix}ytmp4
+  ├─ Mas link del video de YT
+  ├─ ❏ ${prefix}ytmp3
+  └─ Mas link del video de YT
+
+◪ *AUDIO*
+  │
+  ├─ ❏ ${prefix}play
+  ├─ Mas nombre y artista
+  ├─ ❏ ${prefix}bahasa
+  └─ ❏ ${prefix}tts es
+
+◪ *OTROS*
+  │
+  ├─ ❏ ${prefix}wame
+  ├─ Link de Whatsapp
+  ├─ ❏ ${prefix}troleo
+  ├─ trolea a tus panas
+  ├─❏ ${prefix}qrcode
+  └─ Coloca un texto
+
+◪ *GRUPOS*
+  │
+  ├─ ❏ ${prefix}closecg
+  ├─ Cerrar el grupo solo admins
+  ├─ ❏ ${prefix}opencg
+  ├─ Abrir grupo solo admins
+  ├─ ❏ ${prefix}kick o pafuera
+  ├─ Eliminar a un miembro 
+  ├─ ❏ ${prefix}promote
+  ├─ Dar admin a un miembro
+  ├─ ❏ ${prefix}demote
+  ├─ Quitar el admin
+  ├─ ❏ ${prefix}linkgc
+  ├─ Link del grupo
+  ├─ ❏ ${prefix}tagall
+  └─ Nombra a todos los del grupo
+  
+◪ *NSWF NUEVAS FUNCIONES* 
+  │
+  └─ ❏ ${prefix}nsfwmenu
+
+
+𝕤𝕒𝕞𝕦𝕣𝕒𝕚 𝕓𝕠𝕥
+`
+}
+
+exports.help = help
---
+++ src/nsfwmenu.js
@@ -0,0 +1,14 @@
+const nsfwmenu = (prefix, pushname) => {
+    return `Comandos para ver pornito
+    ◪ *NSFW*
+  │
+  ├─ ❏ ${prefix}nsfwbobs
+  ├─ ❏ ${prefix}nsfwsidebobs
+  ├─ ❏ ${prefix}nsfwahegao
+  └─ ❏ ${prefix}nsfwfeets
+
+𝕤𝕒𝕞𝕦𝕣𝕒𝕚 𝕓𝕠𝕥`
+
+}
+
+exports.nsfwmenu = nsfwmenu
